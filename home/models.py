544a9a4109046295e6f5d46c061d071463ab5883
HoggyHog <93634120+HoggyHog@users.noreply.github.com>
2022-05-23 20:21:56 +0530

    first commit

---
+++ home/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Contact(models.Model):
+    name=models.CharField(max_length=100,default="")
+    email=models.CharField(max_length=100,default="")
+    roll=models.CharField(max_length=100,default="")
+    phone=models.DecimalField(max_digits=10,decimal_places=0)
+    dept=models.CharField(max_length=100,default="")
+
+    web=models.CharField(max_length=100,default=" ",null=" ")
+    ui=models.CharField(max_length=100,default=" ",null=" ")
+    business=models.CharField(max_length=100,default=" ",null=" ")
+
+    comments=models.CharField(max_length=100,default="")
+    
+    def __str__(self):
+            return self.name
---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,HttpResponse
+from home.models import Contact
+
+def index(request):
+    if(request.method=="POST"):
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        roll=request.POST.get('roll')
+        phone=request.POST.get('phone')
+        dept=request.POST.get('dept')
+
+        web=request.POST.get('web')
+        ui=request.POST.get('ui')
+        business=request.POST.get('business')
+
+        comments=request.POST.get('comments')
+
+        contact=Contact(name=name,email=email,roll=roll,phone=phone,dept=dept,web=web,ui=ui,business=business,comments=comments)
+        contact.save()
+
+        
+
+    return render(request,'index.html')
